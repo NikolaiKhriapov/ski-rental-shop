baeb610cd9ae7de02b108dbd94b72481dc162fc0
Nikolai Khriapov <kolyakhryapov@gmail.com>
2022-11-09 20:42:56 +0600

    1. Finished implementing pages for broken equipment and equipment in service 3. Minor changes

--- src/main/java/my/project/skirentalshop/controller/EquipmentController.java
+++ src/main/java/my/project/skirentalshop/controller/EquipmentController.java
@@ -1,18 +1,17 @@
 package my.project.skirentalshop.controller;
 
-import my.project.skirentalshop.model.Equipment;
-import my.project.skirentalshop.model.enums.TypesOfEquipment;
 import my.project.skirentalshop.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
+import static my.project.skirentalshop.model.enums.EquipmentCondition.BROKEN;
+import static my.project.skirentalshop.model.enums.EquipmentCondition.SERVICE;
 
 @Controller
-@RequestMapping("/admin/equipment/{type}")
+@RequestMapping("/admin/equipment")
 public class EquipmentController {
 
     private final EquipmentService equipmentService;
@@ -22,93 +21,19 @@ public class EquipmentController {
         this.equipmentService = equipmentService;
     }
 
-    @ModelAttribute
-    public void addToModel(@PathVariable("type") String type, Model model) {
-        model.addAttribute("typeOfEquipment", TypesOfEquipment.convertToEnumField(type));
-    }
-
-    // ----- show all -----
-    @GetMapping
-    public String showAll(@PathVariable("type") String type, Model model) {
-        model.addAttribute("action", "showAll");
-        model.addAttribute("listOfEquipment", equipmentService.showAllEquipment(type));
+    // ----- show broken equipment -----
+    @GetMapping("/broken")
+    public String showBroken(Model model) {
+        model.addAttribute("equipmentCondition", BROKEN);
+        model.addAttribute("listOfEquipment", equipmentService.showEquipmentByCondition(BROKEN));
         return "admin/equipment/equipment";
     }
 
-    // ----- add new -----
-    @GetMapping("/new")
-    public String create(@PathVariable("type") String type,
-                         Model model) {
-        model.addAttribute("action", "create");
-        model.addAttribute("equipment", equipmentService.createNewEquipmentByType(type));
-        return "admin/equipment/equipment";
-    }
-
-    @PostMapping
-    public String create(@PathVariable("type") String type,
-                         @ModelAttribute("equipment") @Valid Equipment oneEquipment,
-                         BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("action", "create");
-            return "admin/equipment/equipment";
-        }
-        equipmentService.addNewEquipmentToDB(oneEquipment);
-        return "redirect:/admin/equipment/" + type;
-    }
-
-    // ----- edit -----
-    @GetMapping("/{equipmentId}")
-    public String showOne(@PathVariable("type") String type,
-                          @PathVariable("equipmentId") Long equipmentId, Model model) {
-        model.addAttribute("action", "update");
-        model.addAttribute("equipment",
-                equipmentService.showOneEquipmentById(equipmentId, type));
-        return "admin/equipment/equipment";
-    }
-
-    @PatchMapping("/{equipmentId}")
-    public String update(@PathVariable("type") String type,
-                         @PathVariable("equipmentId") Long equipmentId,
-                         @ModelAttribute("equipment") @Valid Equipment updatedEquipment,
-                         BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("action", "update");
-            return "admin/equipment/equipment";
-        }
-        equipmentService.updateEquipmentById(equipmentId, updatedEquipment, type);
-        return "redirect:/admin/equipment/" + type;
-    }
-
-    // ----- delete -----
-    @DeleteMapping("/{equipmentId}")
-    public String delete(@PathVariable("type") String type,
-                         @PathVariable("equipmentId") Long equipmentId) {
-        equipmentService.deleteEquipmentById(equipmentId);
-        return "redirect:/admin/equipment/" + type;
-    }
-
-    // ----- search -----
-    @GetMapping("/search")
-    public String showAllBySearch(@PathVariable("type") String type,
-                                  @RequestParam("search") String search,
-                                  Model model) {
-        model.addAttribute("action", "search");
-        model.addAttribute("listOfEquipment", equipmentService.showEquipmentBySearch(search, type));
-        model.addAttribute("search", search);
-        return "admin/equipment/equipment";
-    }
-
-    // ----- sort -----
-    @GetMapping("/sort")
-    public String sortAllByParameter(@PathVariable("type") String type,
-                                     @RequestParam("parameter") String parameter,
-                                     @RequestParam("sortDirection") String sortDirection,
-                                     Model model) {
-        model.addAttribute("action", "showAll");
-        model.addAttribute("reverseSortDirection", sortDirection.equals("asc") ? "desc" : "asc");
-        model.addAttribute("listOfEquipment", equipmentService.sortAllEquipmentByParameter(
-                parameter, sortDirection, type)
-        );
+    // ----- show equipment in service -----
+    @GetMapping("/in-service")
+    public String showInService(Model model) {
+        model.addAttribute("equipmentCondition", SERVICE);
+        model.addAttribute("listOfEquipment", equipmentService.showEquipmentByCondition(SERVICE));
         return "admin/equipment/equipment";
     }
 }
